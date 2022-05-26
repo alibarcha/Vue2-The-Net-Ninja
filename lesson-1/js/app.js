5459ef4fbaffe9c749a4392686d00ca8993214c4
Ali Haider <alibarcha910@gmail.com>
2022-05-25 18:20:28 -0700

    first commit

---
+++ lesson-1/js/app.js
@@ -0,0 +1,27 @@
+// ---------first vue js--------
+      // vue instance
+new Vue({
+      // bind root element
+  el:'#vue-app',
+      //   data
+  data:{
+      title:'Vue js Title',
+      subTitle:'Subtitle of Vue js'
+  }
+});
+
+
+
+
+/*
+//  -- syntax--
+new Vue({
+    el:'#vue-app',
+    data:{
+
+    },
+    components:{
+
+    }
+});
+*/
