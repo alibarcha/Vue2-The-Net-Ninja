3d81fc4c6977101b24c138d8fe7c683c7d4d1622
Ali Haider <alibarcha910@gmail.com>
2022-05-25 21:54:27 -0700

    Data Binding in vue js

---
+++ lesson-2-2/js/app.js
@@ -0,0 +1,22 @@
+
+// vue instance-
+
+new Vue({
+   el:'#vue-app',
+    
+    // data
+    data:{
+        name:'barcha',
+        title:'Wellcome To Vue js ',
+        url:'https://v2.vuejs.org/v2/guide/index.html',
+        subTitle:'this is subtitle of vue',
+        inner:'<span>span tag content</span>',
+        // inner:'span tag content'
+        websiteTag:'<a href="https://v2.vuejs.org/v2/guide/index.html">website tag bind</a>'
+    },
+
+    // functions
+    methods:{
+
+    }
+});
