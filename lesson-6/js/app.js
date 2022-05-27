0d2c06fa88573f9b520b629eb53284ebbbf6b434
Ali Haider <alibarcha910@gmail.com>
2022-05-26 23:57:39 -0700

    conditions and looping

---
+++ lesson-6/js/app.js
@@ -0,0 +1,30 @@
+
+new Vue({
+    el:'#vue-app',
+
+    data:{
+        error:false,
+        success:false,
+        good:true,
+        bad:false,
+
+        skills:['html','css','javascript','vue','react'],
+
+        cars:{
+            name:'vitz',
+            color:'red',
+            model:2022,
+            recommend:true
+        }
+
+    },
+
+    methods:{
+
+    },
+
+    computed:{
+
+    }
+
+});
