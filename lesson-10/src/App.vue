fe46c9b834843e8e4279aee4ec683b7c8756dfc3
Ali Haider <alibarcha910@gmail.com>
2022-05-31 18:51:17 -0700

    buil simple components

---
+++ lesson-10/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- -- root element -- -->
+  <div id="app">
+    <app-header></app-header>
+    <!-- <HeaderArea/> -->
+    <NinjaArea/>
+
+    <FooterArea/>
+   
+  </div>
+</template>
+
+<script>
+import  HeaderArea from "./components/HeaderArea.vue"
+import FooterArea from "./components/FooterArea.vue"
+import NinjaArea from "./components/NinjaArea.vue"
+
+
+
+export default {
+  name: "App",
+  components: {
+   'app-header': HeaderArea,
+  // HeaderArea,
+  FooterArea,
+  NinjaArea,
+   
+},
+};
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap');
+body{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+}
+
+
+</style>
---
+++ lesson-10/src/components/FooterArea.vue
@@ -0,0 +1,45 @@
+<template>
+    <footer>
+        <p><a v-bind:href="link">{{copyright}}</a> </p>
+    </footer>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'FooterArea',
+    data (){
+        return {
+            copyright:'Copyright 2022 The Net Ninja',
+            link:'https://www.youtube.com/watch?v=6-us2D7GQCk&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=24',
+
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+
+}
+</script>
+
+<style scoped>
+footer{
+    background-color: #222;
+    padding: 15px;
+}
+p{
+
+    text-align: center;
+}
+p a{
+    color: white;
+}
+p a:hover{
+    color: lightgreen;
+}
+
+</style>
