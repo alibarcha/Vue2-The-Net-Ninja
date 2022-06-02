37f1e67c34f6cb8da05d262d9a09d17cff4b8551
Ali Haider <alibarcha910@gmail.com>
2022-06-01 20:37:54 -0700

    input Binding in vue

---
+++ lesson-14/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- -- root element -- -->
+  <div id="app">
+    
+    <add-blog>
+      dfdk
+    </add-blog>
+
+   
+   
+
+  </div>
+</template>
+
+<script>
+
+import AddBlog from './components/AddBlog.vue'
+export default {
+  name: "App",
+  components: {
+    'add-blog':AddBlog,
+
+
+},
+ data(){
+   return {
+     
+   }
+ }
+
+};
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap');
+body{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
