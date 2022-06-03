65a010fe99d57d2ee332c735c4f39c026985a2ca
Ali Haider <alibarcha910@gmail.com>
2022-06-03 01:13:56 -0700

    routing

---
+++ lesson-22/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- -- root element -- -->
+  <div id="app">
+    
+    <router-view></router-view>
+  
+
+  </div>
+</template>
+
+<script>
+
+import ShowBlog from './components/ShowBlog.vue'
+import AddBlog from './components/AddBlog.vue'
+
+export default {
+  name: "App",
+  components: {
+    ShowBlog,
+    AddBlog
+  
+},
+ data(){
+   return {
+     
+     
+   }
+ },
+
+
+};
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap');
+body{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
---
+++ lesson-22/src/routes.js
@@ -0,0 +1,7 @@
+import ShowBlog from './components/ShowBlog.vue'
+import AddBlog from './components/AddBlog.vue'
+
+export default[
+    {path:'/',component:ShowBlog},
+    {path:'/add',component:AddBlog}
+]
